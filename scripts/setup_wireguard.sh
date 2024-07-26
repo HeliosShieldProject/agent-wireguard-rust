89c613885f2220969e2f03708d30bee83028db57
Eugene Michkov <michkov.evgeny@gmail.com>
2024-07-26 13:38:37 +0300

    add setup script

---
+++ scripts/setup_wireguard.sh
@@ -0,0 +1,16 @@
+source .env
+apt update -y && apt install -y wireguard
+wg genkey | tee /etc/wireguard/privatekey | wg pubkey | tee /etc/wireguard/publickey
+chmod 600 /etc/wireguard/privatekey
+echo "[Interface]
+PrivateKey = $(cat /etc/wireguard/privatekey)
+Address = $WIREGUARD_ADDRESS
+ListenPort = $WIREGUARD_PORT
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+" > /etc/wireguard/wg0.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+systemctl enable wg-quick@wg0.service
+systemctl start wg-quick@wg0.service
+systemctl status wg-quick@wg0.service
